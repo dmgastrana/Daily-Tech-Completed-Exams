1ea02a46aa72e6298417bd94f8fa46109a6c5ffe
dmgastrana <dmgastrana@gmail.com>
2026-09-08 18:16:37 -0700

    Update ECG to replace EKG for SYN San Gabriel

--- daily.js
+++ daily.js
@@ -15,7 +15,7 @@ const FACILITY_MODALITIES = {
     "DMG City of Industry": ["CT", "Bone Density", "MG", "MR", "US", "XR"],
     "DMG Monterey Park": ["CT", "Bone Density", "MG", "MR", "US", "XR"],
     "DMG San Gabriel": ["CT", "Bone Density", "ECG", "MG", "MR", "US", "XR"],
-    "SYN San Gabriel": ["CT", "EKG", "MR", "US", "XR"]
+    "SYN San Gabriel": ["CT", "ECG", "MR", "US", "XR"]
 };
 
 /* ============================================================
